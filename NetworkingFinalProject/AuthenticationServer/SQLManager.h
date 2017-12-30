34321b4376b63a6cda9f4dbdcfe8cc3e5a6ecc61
WesBach <wes_bach@live.com>
2017-12-29 20:31:42 -0500

    Added the sql and openssl libs as well as headers.

--- NetworkingFinalProject/AuthenticationServer/SQLManager.h
+++ NetworkingFinalProject/AuthenticationServer/SQLManager.h
@@ -1,5 +1,11 @@
 #ifndef _SQL_MANAGER_HG_
 #define _SQL_MANAGER_HG_
+#include <string>
+#include <cppconn\driver.h>
+#include <cppconn\exception.h>
+#include <cppconn\resultset.h>
+#include <cppconn\statement.h>
+#include <cppconn\prepared_statement.h>
 
 class SQLManager {
 public:
@@ -7,7 +13,13 @@ public:
 	SQLManager();
 	~SQLManager();
 
-
+	bool execute(const std::string& statement);
+	int executeUpdate(const std::string& statement);
+private:
+	sql::Statement* statement;
+	sql::PreparedStatement* prepState;
+	sql::Driver* driver;
+	sql::Connection* connection;
 };
 #endif // !_SQL_MANAGER_HG_
 
